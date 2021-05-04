79812552341bab780c5299545b98417df9e77c45
Mohsin853 <61412070+Mohsin853@users.noreply.github.com>
2021-05-04 06:29:43 +0530

    Update companyTest.py

--- leadbookassignment/leadbookassignment/spiders/companyTest.py
+++ leadbookassignment/leadbookassignment/spiders/companyTest.py
@@ -1,18 +1,18 @@
-from scrapytest.tests import Type
-from ..items import LeadbookassignmentItem
+# from scrapytest.tests import Type
+# from ..items import LeadbookassignmentItem
 
 
-class TestPost(LeadbookassignmentItem):
-    # defining item that is being covered
-    item_cls = LeadbookassignmentItem
+# class TestPost(LeadbookassignmentItem):
+#     # defining item that is being covered
+#     item_cls = LeadbookassignmentItem
 
-    # defining field tests
-    company_name = Type(str)
-    source_url = Type(str)
+#     # defining field tests
+#     company_name = Type(str)
+#     source_url = Type(str)
     
 
-    # also supports methods!
-    def url_test(self, value: str):
-        if not value.startswith('http'):
-            return f'Invalid url: {value}'
-        return ''
+#     # also supports methods!
+#     def url_test(self, value: str):
+#         if not value.startswith('http'):
+#             return f'Invalid url: {value}'
+#         return ''
